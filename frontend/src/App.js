1d682f28b2b093f16a1096707aede7406ab5e968
Diego Gamboa <difegam3@gmail.com>
2022-03-23 20:42:11 +0100

    Controlled search componet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+
 
 const App = () => {
+
+  const [search, setSearch] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e);
+    // console.log(e.target[0].value);
+    console.log(search);
+  };
+
+  // console.log(search);
+
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search />
+      <Search search={search} setSearch={setSearch} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = ({ search, handleChange }) => {
+const Search = ({search , setSearch, handleSubmit}) => {
   return (
     <Container className="mt-3">
       <Row className="justify-content-md-center">
         <Col xs={12} md={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search new image..." />
+                <Form.Control
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search new image..." />
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
